c60a448478ef01c106be378f4f4bd6ffb61dae47
DESKTOP-27TANMS\Ela <elahe.fahmideh@gmail.com>
2024-09-02 19:28:15 +0330

    feat:add select for choose category

---
+++ frontend/src/ui/selectInput.jsx
@@ -0,0 +1,21 @@
+function SelectInput({ label, options, name, required, register }) {
+  return (
+    <div className="mb-2">
+      <label
+        htmlFor={name}
+        className="block mb-2 mt-3 text-secondary-200 md:text-secondary-500"
+      >
+        {label} {required && <span className="text-error">*</span>}
+      </label>
+      <select id={name} className="input_field w-full" {...register(name)}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default SelectInput;
