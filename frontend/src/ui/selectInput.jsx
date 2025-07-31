8e45c917222863116738c27f7f29b646b568897b
DESKTOP-RD2LRN2\Elahe <elahe.fahmideh@gmail.com>
2025-07-31 16:15:26 +0330

    chore: add default value prop

--- frontend/src/ui/selectInput.jsx
+++ frontend/src/ui/selectInput.jsx
@@ -1,13 +1,25 @@
-function SelectInput({ label, options, name, required, register }) {
+function SelectInput({
+  label,
+  options,
+  name,
+  required,
+  register,
+  defaultValue = {},
+}) {
   return (
     <div className="mb-2">
       <label
         htmlFor={name}
-        className="block mb-2 mt-3 text-secondary-200 md:text-secondary-500"
+        className="block text-right mb-2 mt-3 text-secondary-200 md:text-secondary-500"
       >
         {label} {required && <span className="text-error">*</span>}
       </label>
-      <select id={name} className="input_field w-full" {...register(name)}>
+      <select
+        id={name}
+        className="input_field w-full"
+        defaultValue={defaultValue}
+        {...register(name)}
+      >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
