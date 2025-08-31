dee460c37e8d528833d96d78ef2ba6d5d0ab07a7
DESKTOP-RD2LRN2\Elahe <elahe.fahmideh@gmail.com>
2025-08-31 16:37:19 +0330

    feat: handle return back user when is login

--- frontend/src/features/authentication/Layer.jsx
+++ frontend/src/features/authentication/Layer.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SendOtpForm from "./SendOtpForm";
 import CheckOtpForm from "./CheckOtpForm";
 import { useMutation } from "@tanstack/react-query";
 import { getOtp } from "@/services/authService";
 import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
-import DarkModeToggle from "../../ui/darkModeToggle";
+import DarkModeToggle from "@/ui/darkModeToggle";
+import useUser from "./useUser";
+import { useNavigate } from "react-router-dom";
 
 function Layer() {
   const [step, setStep] = useState(1);
   const [otp, setOtp] = useState("");
-
   const { register, handleSubmit, getValues } = useForm();
+  const { user } = useUser();
+  const navigate = useNavigate();
 
   const {
     isPending,
@@ -62,6 +65,10 @@ function Layer() {
         return null;
     }
   };
+
+  useEffect(() => {
+    if (user) navigate(-1);
+  }, [user, navigate]);
 
   return (
     <>
